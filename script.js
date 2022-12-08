fc9c362dc95186a8c9787907eeca48672f09a45b
Misha <misharexviashvili@yahoo.com>
2022-12-09 01:10:05 +0400

    task 2

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Task 1
 const replacer = function (string, valueToReplace, valueToReplaceWith) {
   const arr = string.trim().split(" ");
+  //   Finds index of replacable word
   const replaceWordIndex = arr.indexOf(valueToReplace);
+  //   Replace word with new value using array = method
   arr[replaceWordIndex] = valueToReplaceWith;
+  //   Turn array elements into 1 sentence
   const newSentece = arr.join(" ");
   return newSentece;
+};
+
+// Task 2
+const capitalizer = function (string) {
+  //  Finds index of replacable word
+  const arr = string.trim().split(" ");
+  const array = [];
+  //  Concatenates uppercased first character and rest of the word, then pushes words into an array
+  for (const element of arr) {
+    const capitlized = element.charAt(0).toUpperCase() + element.slice(1);
+    array.push(capitlized);
+  }
+  //   Turn array elements into 1 sentence
+  const capitalizedSentence = array.join(" ");
+  return capitalizedSentence;
 };
